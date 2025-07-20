620a148d57939e752f5f418b8a55d3175a0c603b
kukati pujitha <142659410+kukatipujitha@users.noreply.github.com>
2025-07-20 07:37:17 +0530

    Day2_Variables.java

---
+++ Day2_Variables.java
@@ -0,0 +1,17 @@
+class Day2_Variables {
+    public static void main(String[] args) {
+        // Declaring and initializing variables
+        String name = "Pujitha";
+        int age = 20;
+        double height = 5.4;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        // Printing variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " feet");
+        System.out.println("Grade: " + grade);
+        System.out.println("Are you a student? " + isStudent);
+    }
+}
