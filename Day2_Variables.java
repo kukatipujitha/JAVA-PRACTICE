a710776b410d2073abc7f9903e795125a6189a91
kukati pujitha <142659410+kukatipujitha@users.noreply.github.com>
2025-07-21 20:10:01 +0530

    Day2_VariablesAndOperators.java

--- Day2_Variables.java
+++ Day2_Variables.java
@@ -15,3 +15,52 @@ class Day2_Variables {
         System.out.println("Are you a student? " + isStudent);
     }
 }
+//---------------------------------------------------------------------------------------------------------------------
+//Arithmatic operations
+class main{
+    public static void main(String[] args){
+        int num1=20;
+        int num2=10;
+        System.out.println("sum is :"+(num1+num2));
+        System.out.println("diff is :"+(num1-num2));
+        System.out.println("Multiplication is:"+(num1*num2));
+        System.out.println("division is :"+(num1/num2));
+    }
+}
+
+//-----------------------------------------------------------------------------------------------
+//Realtional operators,,,,,ternary operator,,,Assignment operator
+class Main {
+    public static void main(String[] args) {
+       String st1="one";
+       String st2="two";
+       int num1=20;
+       int num2=10;
+       boolean res=(st1==st2)? true:false;
+       System.out.println(res);
+       boolean res1=(st1!=st2)? true:false;
+       System.out.println(res1);
+       boolean res2=(num1>=num2)? true:false;
+       System.out.println(res2);
+       boolean res3=(num1<=num2)? true:false;
+       System.out.println(res3);
+       num1+=num2;
+       System.out.println(num1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
