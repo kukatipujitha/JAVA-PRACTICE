ca4dda3edb34b7c4728219d83c3b1b5d313aaf63
kukati pujitha <142659410+kukatipujitha@users.noreply.github.com>
2025-07-27 20:35:36 +0530

    Day6_ConditionalFlow.java

--- Day6_ConditionalFlow.java
+++ Day6_ConditionalFlow.java
@@ -136,66 +136,7 @@ enter your roll  number:
 vyshu
 
 =============================================================================================== 
-//while-- to print natural number if enter number is greater than 0;
 
-import java.util.Scanner;
-class While {
-    public static void main(String [] args){
-        Scanner input=new Scanner(System.in);
-        int i=1;
-        System.out.println("enter a number");
-        int num=input.nextInt();
-        while (i<=num){
-            System.out.println(i);
-            i+=1;
-        }
 
-        
-    }
-}
-compiler output----
-
-enter a num
-12
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-=============================================================
-//do while--The do-while loop executes the code block once before checking the condition. It guarantees at least one execution of the loop body.
-import java.util.Scanner;
-class DoWhileExample {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int num;
-
-        do {
-            System.out.print("Enter a positive number (enter 0 to stop): ");
-            num = input.nextInt();
-            System.out.println("You entered: " + num);
-        } while (num != 0);
-
-        System.out.println("Loop ended because you entered 0.");
-    }
-}
-----compiler output----
-Enter a positive number (enter 0 to stop): 9
-You entered: 9
-Enter a positive number (enter 0 to stop): 5
-You entered: 5
-Enter a positive number (enter 0 to stop): 2
-You entered: 2
-Enter a positive number (enter 0 to stop): 0
-You entered: 0
-Loop ended because you entered 0.
-================================================================
    
     
