589360fa922764cac1fdcbed64f8fedd54a67279
kukati pujitha <142659410+kukatipujitha@users.noreply.github.com>
2025-08-09 15:54:15 +0530

    Update Day5_ArrayChallenges.java

--- Day5_ArrayChallenges.java
+++ Day5_ArrayChallenges.java
@@ -27,3 +27,53 @@ class NegativePositive{
 positives are :[10, 4, 7]
 negatives are :[-2, -4, -3]
 =========================================================
+    //separate positives and negatives in array by user input
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class NegativePositive{
+    public static void main(String [] args){
+      Scanner scan=new Scanner(System.in);
+          System.out.println("enter the length of the array:");
+
+      int n=scan.nextInt();
+      int[] Arr=new int[n];
+      System.out.println("enter the elements of the array:");
+
+      for(int i=0;i<n;i++){
+
+          Arr[i]=scan.nextInt();
+      }
+
+      ArrayList<Integer> positiveList=new ArrayList<>();
+      ArrayList<Integer> negativeList=new ArrayList<>();
+      ArrayList<Integer> zeroesList=new ArrayList<>();
+      
+    for (int i=0;i<Arr.length;i++){
+        if (Arr[i]<0){
+              negativeList.add(Arr[i]);        }
+        else if (Arr[i]>0){
+            positiveList.add(Arr[i]);
+        }
+        else {
+            zeroesList.add(Arr[i]);
+        }
+      
+    }
+    System.out.println("positives are :"+ positiveList);
+    System.out.println("negatives are :"+ negativeList);
+    System.out.println("zeroes are :"+ zeroesList);
+    
+}
+}
+----------compiler output-----
+enter the length of the array:
+5
+enter the elements of the array:
+-2 0 -3 7 8
+positives are :[7, 8]
+negatives are :[-2, -3]
+zeroes are :[0]
+
+========================================
