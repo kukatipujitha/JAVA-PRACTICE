2994b3858913cf48f35aace4667bc88af3b94cd0
kukati pujitha <142659410+kukatipujitha@users.noreply.github.com>
2025-08-10 15:05:28 +0530

    Update Day5_ArrayChallenges.java

--- Day5_ArrayChallenges.java
+++ Day5_ArrayChallenges.java
@@ -77,3 +77,41 @@ negatives are :[-2, -3]
 zeroes are :[0]
 
 ========================================
+//to seperate odd and even numbers in array with user input
+import java.util.Scanner;
+import java.util.ArrayList;
+class main{
+    public static void main(String[] args){
+        // int[] Arr=new int[4];
+        // int[] Arr={1,2,3,4};
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter the length of array");
+        int n=scan.nextInt();
+        int[] Arr=new int[n];
+        System.out.println("enter the numbers of array");
+        for (int i =0;i<n;i++){
+            // int[] Arr=new int[n];
+            Arr[i]=scan.nextInt();
+        }
+        ArrayList<Integer> oddNum=new ArrayList<>();
+        ArrayList<Integer> evenNum=new ArrayList<>();
+        for (int i=0;i<(n);i++){
+            if (Arr[i]%2==0){
+                evenNum.add(Arr[i]);}
+            else{
+                oddNum.add(Arr[i]);            }
+                
+            }
+           System.out.println("even number in array are:"+evenNum) ;
+           System.out.println("odd number in array are:"+oddNum) ;
+        }
+    }
+------------compiler output-------------
+enter the length of array
+5
+enter the numbers of array
+1 45 78 43 -5
+even number in array are:[78]
+odd number in array are:[1, 45, 43, -5] 
+===============================================
+
